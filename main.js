765c9cccd7d5924f2c22237b768e60eb3dd87461
Rahul Dutta <79462696+rahulddutta@users.noreply.github.com>
2021-06-28 23:06:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
+let elements = document.getElementsByName("cssPro");
+let div = document.getElementById("modify");
+
 function set() {
-    let elements = document.getElementsByName('cssPro');
+  for (let index = 0; index < elements.length; index++) {
+    let property = elements[index].getAttribute("id");
+    let cssValue = elements[index].value;
 
-    for(let index = 0; index < elements.length; index++) {
-       let cssProperty = elements[index].getAttribute('id');
-
-        let cssValue = elements[index].value;
-
-       // console.log(cssProperty, cssValue);
-
-       let div = document.getElementById('modify');
-
-       div.style[cssProperty] = cssValue;
-
-    }
+    div.style[property] = cssValue;
+  }
 }
 
-document.getElementById('set').addEventListener('click', set);
+document.getElementById("set").addEventListener("click", set);
